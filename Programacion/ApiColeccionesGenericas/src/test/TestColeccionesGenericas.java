1c989e67c9f6ea14faefe8d44ba60d8f3b971a99
rosanchez <rodrigojesus-sanchez@aj.com.py>
2021-04-24 16:18:52 -0400

    Colecciones con clases genericas

---
+++ Programacion/ApiColeccionesGenericas/src/test/TestColeccionesGenericas.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.*;
+
+/**
+ *
+ * @author Rodrigo-DevCode
+ */
+public class TestColecciones {
+
+    public static void main(String[] args) {
+
+        //Lista guarda el orden en el que se insertaron los elmentos, y es un poco mas lento por eso, tambien permite repetir datos
+        List miLista = new ArrayList();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miercoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Viernes");
+
+        //imprimir(miLista);
+        //Set no guarda el orden, es mas rapido, y no permite datos duplicados, simplemente los desecha
+        Set miSet = new HashSet();
+        miSet.add("Lunes");
+        miSet.add("Martes");
+        miSet.add("Miercoles");
+        miSet.add("Jueves");
+        miSet.add("Viernes");
+        miSet.add("Viernes");
+
+        //  imprimir(miSet);
+        //map no hereda de Collection si no de la clase Map
+        Map miMap = new HashMap();
+        miMap.put("1", "Rodrigo");
+        miMap.put("2", "Diana");
+        miMap.put("3", "Johan");
+        miMap.put("4", "Jhoel");
+
+        String nombre = (String) miMap.get("1");
+        System.out.println("nombre = " + nombre);
+        //imprimir todas las llaves
+        imprimir(miMap.keySet());//devuleve un set
+        //imprimir todos los valores
+        imprimir(miMap.values());//values devuelve una coleccion
+    }
+
+    private static void imprimir(Collection coleccion) {
+        /*for (Object object : coleccion) {
+            System.out.println("object = " + object);
+        }*/
+
+        coleccion.forEach(elemento -> {
+            System.out.println("elemento = " + elemento);
+        });
+    }
+}
